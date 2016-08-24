a519831073ef779dc77041dde4181884bc89a257
Ellen Bartling <ellen@emergentorder.com>
2016-08-24 13:48:57 -0500

    Adding extra credit star to tags

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -3,13 +3,19 @@
     $count = 0;
     if ($posttags) {
       echo '<div class="tags">';
-        foreach($posttags as $tag) {
-            $count++;
-            echo '<span><a data-filter="' . $tag->slug . '" href="#">'. $tag->name .'</a> </span>';
-            if( $count > 3 ) break;
-        }
         if (get_post_type($post->ID) == "extra-credit") {
-          echo '<span><a data-filter="extra-credit" href="#">extra credit</a></span>';
+          foreach($posttags as $tag) {
+              $count++;
+              echo '<span><a class="extra-credit-star" data-filter="' . $tag->slug . '" data-filter="extra-credit" href="#">Extra Credit: '. $tag->name .'</a> </span>';
+              if( $count > 3 ) break;
+          }
+          // echo '<span><a data-filter="extra-credit" href="#">extra credit</a></span>';
+        } else {
+          foreach($posttags as $tag) {
+              $count++;
+              echo '<span><a data-filter="' . $tag->slug . '" href="#">'. $tag->name .'</a> </span>';
+              if( $count > 3 ) break;
+          }
         }
       echo '</div>';
     }
